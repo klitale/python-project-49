01018977b89ab070737b9b5e4d7aeb298480c9d8
volkovdamsw@gmail.com <elias_aioli@Daniils-MacBook-Pro.local>
2023-09-17 12:17:37 +0300

    fix gcd

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -5,11 +5,11 @@ DESCRIPTION = 'Find the greatest common divisor of given numbers.'
 
 
 def get_question():
-    number_one = random.randint(0, 50)
-    number_two = random.randint(0, 50)
+    number_one = random.randint(1, 50)
+    number_two = random.randint(1, 50)
     question = f'{number_one} {number_two}'
     answer = str(get_gcd(number_one, number_two))
-    return question, answer
+    return answer, question
 
 
 def get_gcd(one, two):
