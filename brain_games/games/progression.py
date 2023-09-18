12fd7f115b36cd2b3cf833bc041cc3e93bf1aa11
volkovdamsw@gmail.com <elias_aioli@Daniils-MacBook-Pro.local>
2023-09-18 13:17:07 +0300

    fix progression

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -11,7 +11,7 @@ def get_question():
     hidden_position = random.randint(0, PROGRESSION_LENGTH - 1)
     progression = get_progression(start, step, PROGRESSION_LENGTH)
     right_answer = progression[hidden_position]
-    progression[hidden_position] = '.. '
+    progression[hidden_position] = '..'
     question = " ".join(progression)
     return right_answer, question
 
